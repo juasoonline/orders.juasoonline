16be8b77a9ce49178738975f113b5b6250fe9bfa
vagrant <vagrant@orders.juasoonline.dev>
2021-05-03 13:37:06 +0000

    CustomerOnserver: resource

--- app/Observers/Customer/CustomerObserver.php
+++ app/Observers/Customer/CustomerObserver.php
@@ -2,7 +2,15 @@
 
 namespace App\Observers\Customer;
 
+use App\Models\Customer\Customer;
+
 class CustomerObserver
 {
-    //
+    /**
+     * @param Customer $customer
+     */
+    public function creating( Customer $customer )
+    {
+        $customer -> resource_id = uniqid();
+    }
 }
